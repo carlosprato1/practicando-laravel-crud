a046cce86b9880c2159e3e1725243bcefe8265e2
carlos prato <carlos.prato17@gmail.com>
2019-12-08 00:01:36 -0300

    Configuracion de LARAVEL MIX - instalacion de yarn, nodejs, yarn dev correctamente. - uso de browserSync - actualizacion de componentes.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,13 +3,9 @@
 <head>
 <title>@yield('title','PorDefecto')</title>
 
-<style type="text/css">
-    .active a{
-    	color: red;
-	    text-decoration: none;
-    }
 
-</style>
+<link rel="stylesheet" href="{{mix('/css/app.css')}}">
+<script src="{{mix('/js/app.js')}}"> defer</script>
 
 </head>
 <body>
